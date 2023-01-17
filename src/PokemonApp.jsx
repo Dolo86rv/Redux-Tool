d19610f1f8bb921be2e3b891c3264949b8104c29
Dolorv86 <dolorv86@gmail.com>
2023-01-17 23:46:46 +0100

    Se hace peticion a la API y se muestra lista de pokemons

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,24 +1,33 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from './store/slices/pokemon'
+import {startLoadingPokemons, setPokemons } from './store/slices/pokemon'
 
 export const PokemonApp = () => {
-    
+
     const dispatch = useDispatch()
+    const { isLoading, pokemons, page } = useSelector( state => state.pokemons)
     
     useEffect(()=>{
-        dispatch( getPokemons())
+        dispatch(getPokemons())
     }, [])
     
     return (
         <>
             <h1>PokemonApp</h1>
             <hr />
+            <span>Loading: { isLoading ? 'True' : 'False' }</span>
             <ul>
-                <li>Hola</li>
-                <li>Hola</li>
-                <li>Hola</li>
+                {pokemons.map(item => (
+                    <li key={item.name}>{item.name}</li>
+                ))}
             </ul>
+            <button 
+                disabled= {isLoading}
+                onClick={ ()=> dispatch(getPokemons(page)) }
+            >
+                Next
+            </button>
         </>
     )
 }
